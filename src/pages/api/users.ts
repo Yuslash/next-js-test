3e3519faaf3505d98f68f5c48cd2d287e4d16f5a
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-09-03 13:08:23 +0530

    line breaks

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default async function handler(req: any, res: any) {
+    
     if(req.method === 'POST') {
         const { name , email } = req.body
 
@@ -10,6 +11,7 @@ export default async function handler(req: any, res: any) {
             data: { name, email}
         })
         res.status(200).json(user)
+    
     } else if (req.method === 'GET') {
         const users = await prisma.user.findMany()
         res.status(200).json(users)
